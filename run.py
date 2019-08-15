ee2f78519987ffa73c129a174113ae9a242b53d4
TyphonBak <andrade.czr@gmail.com>
2019-08-15 12:16:56 -0300

    remove port

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from create_app import create_app, socketio
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    #port = int(os.environ.get("PORT", 5000))
     app = create_app(debug=True)
     #app.run(host='0.0.0.0', port=port, debug=True)
-    socketio.run(app, port=port)
+    socketio.run(app)
