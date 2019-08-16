a1d75f6276a70b95ca003cbd642e2f59bb3c2df8
TyphonBak <andrade.czr@gmail.com>
2019-08-16 10:50:25 -0300

    fix deploy command

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from create_app import create_app, socketio
 import os
 
+app = create_app(debug=True)
+socketio.init_app(app)
+
 if __name__ == '__main__':
     #port = int(os.environ.get("PORT", 5000))
-    app = create_app(debug=True)
     #app.run(host='0.0.0.0', port=port, debug=True)
     socketio.run(app)
